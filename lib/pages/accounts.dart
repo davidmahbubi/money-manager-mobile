794ba2421421286adcc85e3ecbcf283903262229
David Mahbubi <ulrichdavid0370@gmail.com>
2022-06-07 19:17:33 +0700

    Added chart, profile, modified icon and label

--- lib/pages/accounts.dart
+++ lib/pages/accounts.dart
@@ -5,8 +5,32 @@ class Accounts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('This is an account page'),
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(top: 20),
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 20),
+            Image.asset(
+              'assets/images/default_profile_picture.png',
+              width: 180,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'David Mahbubi',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Text('ulrichdavid0370@gmail.com'),
+            const SizedBox(height: 40),
+            ElevatedButton(onPressed: () {}, child: const Text('Sign Out'))
+          ],
+        ),
+      ),
     );
   }
 }
